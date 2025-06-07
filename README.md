9f7a19f6021e36b6458e3ad90613e2660f8c0a36
Marco A. Manya <manyamarko@gmail.com>
2025-06-07 01:02:28 -0300

    Creacion del proyecto de la simulacion de Crystales Temporales

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Time_Crystals1
-It is a research project coding in python and other languages like fortran90 and C++
+It is a research project coding in python and other languages like fortran90 and C++. The idea is have several version codes of simulations
---
+++ main.py
@@ -0,0 +1,14 @@
+from config import params
+from models.pauli_matrix import build_pmatrix
+#from models.initial_state import build_initial_state
+#from models.hamiltonian import build_hamiltonian
+#from evolution.evolve import evolve_state
+#from evolution.u_operator import operator_u
+#from observables.magnetization import compute_magnetization
+#from observables.fisher_information import fisher_quantum, fisher_classical
+#from estimation.parameter_estimation import estimate_optimal_parameter
+
+sx = build_pmartix(3,x)
+print("hola")
+
+
