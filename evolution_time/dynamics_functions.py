7adc15752c8d2bd8d78162a0daf7d92f07e3fca1
Marco Manya <manyamarko@gmail.com>
2025-06-21 19:43:07 -0300

    Implementación de la función de operador de evolucion unitario de Floquetñ U_floquet1T(), y U_floquet2T()

--- evolution_time/dynamics_functions.py
+++ evolution_time/dynamics_functions.py
@@ -1,8 +1,8 @@
-from scipy.linalg import expm, eig
+from scipy.linalg import expm
 import numpy as np
 
 
-def U0(H_t, beta, T, t_step):
+def U_floquet1T(H_t, H_kick, beta, T, t_step):
     """
     H_t: Funcion Hamiltoniano dependiente del tiempo
     T: Periodo
@@ -13,9 +13,39 @@ def U0(H_t, beta, T, t_step):
     U0 = np.eye(N, dtype=complex)
     dt = t_step
 
-    for k in range(steps):
+    for k in range(steps+1):
         t = dt * k
         Ht = H_t(t,beta)
+        if t==T:
+            Ht += H_kick
+            print("entro cuando t = T = ", T)
         U0 = expm(-1j * Ht * dt) @ U0
 
     return U0
+
+
+def U_floquet2T(H_t, H_kick, beta, T, t_step):
+    """
+    H_t: Funcion Hamiltoniano dependiente del tiempo
+    T: Periodo
+    t_step: Tamanho para discretizar el tiempo
+    """
+    steps = int(T/t_step)
+    N = H_t(0,beta).shape[0]
+    U0 = np.eye(N, dtype=complex)
+    dt = t_step
+
+    for k in range(2*steps+1):
+        t = dt * k
+        Ht = H_t(t,beta)
+        if t==T and t==2*T:
+            Ht += H_kick
+            print("entro cuando t = T = ", T)
+        U0 = expm(-1j * Ht * dt) @ U0
+
+    return U0
+
+
+
+
+
