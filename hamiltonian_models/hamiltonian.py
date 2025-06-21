d478591873b72cdce79eab641bc18660fcf60910
Marco Manya <manyamarko@gmail.com>
2025-06-21 13:06:23 -0300

    Creación de la  función H_disorder(), termino desordenado del hamiltoniano del sistema

--- hamiltonian_models/hamiltonian.py
+++ hamiltonian_models/hamiltonian.py
@@ -12,3 +12,17 @@ def H_ising(spin_list_matrices):
     sz_list = spin_list_matrices[1:]
 
     return -4* sz0 @ np.sum(sz_list,axis=0)
+
+
+def H_disorder(o_sx, o_sz, disorder_x, disorder_z):
+    """
+    o_sx: List of spin matrices along x-direction
+    o_sz: List of spin matrices along z-direction
+    disorder_x: Disorder vector along x-direction
+    disorder_z: Disorder vector along z-direction\
+    """
+    n = len(disorder_x)
+    suma = np.zeros_like(o_sz[0])
+    for i in range(n):
+        suma += disorder_x[i]*o_sx[i] + disorder_z[i]*o_sz[i]
+    return suma
